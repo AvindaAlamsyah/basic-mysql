7a064e82c8b76ac37dd0b0bcddd9969676a7aa69
AvindaAlamsyah <avindaalamsyah@gmail.com>
2023-07-07 14:11:37 +0700

    feat(alter-table): modify table barang

--- basic_mysql.sql
+++ basic_mysql.sql
@@ -16,3 +16,13 @@ CREATE TABLE barang (
 )  ENGINE=INNODB;
 DESCRIBE barang;
 SHOW CREATE TABLE barang;
+# End create new table
+
+# Edit created table
+ALTER TABLE barang ADD COLUMN deskripsi TEXT;
+ALTER TABLE barang ADD COLUMN test TEXT;
+ALTER TABLE barang DROP COLUMN test;
+ALTER TABLE barang MODIFY id INT NOT NULL;
+ALTER TABLE barang MODIFY nama VARCHAR(200) NOT NULL;
+ALTER TABLE barang MODIFY harga INT NOT NULL DEFAULT 0;
+#End edit create table
